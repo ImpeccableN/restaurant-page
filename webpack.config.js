b57f3cec890da2857404da68969b276b6ea3dc1c
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-04-10 16:42:51 +0200

    fixed typo in config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: ".src/index.js",
+    entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
@@ -13,7 +13,7 @@ module.exports = {
     devServer: {
         watchFiles: ["./src/template.html"],
     },
-    plugin: [
+    plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
