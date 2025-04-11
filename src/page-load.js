393e950dcf9455f5e48fd378d495f31366467e1f
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-04-11 14:18:34 +0200

    implemented loadPage func and imported in index.js

---
+++ src/page-load.js
@@ -0,0 +1,19 @@
+import foodImage from "./img/food_header.jpg"
+
+export const loadPage = function() {
+    console.log("work it");
+    const content = document.querySelector("#content");
+
+    const headline = document.createElement("h1");
+    headline.textContent = "Calippo's!";
+
+    const p = document.createElement("p");
+    p.textContent = "It is food. It is Italian. It is good!";
+
+    const image = document.createElement("img");
+    image.src = foodImage;
+
+    content.appendChild(headline);
+    content.appendChild(p);
+    content.appendChild(image);
+}
